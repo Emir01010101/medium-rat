98c7ceca5772af193e07bed0d48dafbf4bdc4b5e
Ronaldo Aslan <ronaldoaslan29@gmail.com>
2026-03-04 01:48:31 +0300

    fix: tirnak isaretleri ve port ayari duzeltildi

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-const express = require(‘express’); 
-
-const cors = require(‘cors’);
-
+const express = require('express');
+const cors = require('cors');
 const app = express();
 
 app.use(cors());
 
-app.get(“/deneme”,function(req,res){
-
-res.send(“mkdir emirhankarakoc_rat_2023 && cd emirhankarakoc_rat_2023 && echo basarili > mesaj.txt”)
-
+app.get("/deneme", function(req, res) {
+    res.send("mkdir emirhankarakoc_rat_2023 && cd emirhankarakoc_rat_2023 && echo basarili > mesaj.txt");
 });
 
-const port = 2929
+const port = process.env.PORT || 10000;
 
-app.listen(port);
-
-console.log(`Sunucu ${port} portunda calisiyor.`);
+app.listen(port, () => {
+    console.log(`Sunucu ${port} portunda calisiyor.`);
+});
